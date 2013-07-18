ab874cf92288cac7bd8cc8e75bd7a32dbb5ad9a0
Jon DeJong <jon.dejong@gmail.com>
2013-07-17 23:13:56 -0500

    adding line connections to nodes.

--- web-app/js/d3modules/force.js
+++ web-app/js/d3modules/force.js
@@ -4,7 +4,7 @@ forceGlobals.label_group;
 forceGlobals.width = 600;
 forceGlobals.height = 400;
 
-initForceChart = function() {
+initForceChart = function () {
     var chart = d3.select("#container").append("svg")
 //        .attr("class", "chart")
         .attr("width", forceGlobals.width)
@@ -13,11 +13,30 @@ initForceChart = function() {
     return chart;
 }
 
-refreshForce = function(chart, modules) {
-    var color = d3.scale.category20();
-    var transform = "translate(" + forceGlobals.width/2 + "," + forceGlobals.height/2 + ")";
+createGraph = function(modules) {
+    var graph = {};
+    graph.nodes = [];
+    graph.links = [];
 
-    modules.filter( function (element, index, array) {
+    var node = 0;
+    var link = 0;
+    for(var i=0; i<modules.length; i++) {
+        graph.nodes[node++] = {name: modules[i].name, level: modules[i].level}
+        var source = node - 1;
+        for(var j=0; j<modules[i].subModules.length; j++) {
+            graph.nodes[node++] = {name: modules[i].subModules[j], level: modules[i].subModules[j].level};
+            graph.links[link++] = {source: source, target: node - 1};
+        }
+    }
+    return graph;
+}
+
+refreshForce = function (chart, modules) {
+    var graph = createGraph(modules);
+    var color = d3.scale.category20();
+//    var transform = "translate(" + forceGlobals.width / 2 + "," + forceGlobals.height / 2 + ")";
+
+    modules.filter(function (element, index, array) {
             element.x = 0;
             element.y = 0;
             return (element.level > 0);
@@ -25,34 +44,59 @@ refreshForce = function(chart, modules) {
     );
 
     var force = d3.layout.force()
-        .nodes(modules)
-        .charge(function(d) {
+
+        .charge(function (d) {
             return -100 - d.level;
         })
         .size([forceGlobals.width, forceGlobals.height])
+        .linkDistance(40)
+;
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
         .start();
 
+
+        var link = chart.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", "link");
+
     var node = chart.selectAll(".node")
-        .data(modules)
-        .attr("r", function(d) {
-            return d.level;
-        })
+        .data(graph.nodes)
         .enter().
         append("circle")
         .attr("class", "node")
-        .attr("r", function(d) {
-            return d.level;
+        .attr("r", function (d) {
+            return d.level * 1.25;
         })
-//        .style("fill", function(d, i) { return color(i); })
         .attr("fill", function (d, i) {
             return color(i);
         })
         .call(force.drag);
 
-    force.on("tick", function() {
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+    force.on("tick", function () {
+
+        link.attr("x1", function (d) {
+            return d.source.x;
+        })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+
+        node.attr("cx", function (d) {
+            return d.x;
+        })
+            .attr("cy", function (d) {
+                return d.y;
+            });
     });
 
-    force.start();
+//    force.start();
 }
